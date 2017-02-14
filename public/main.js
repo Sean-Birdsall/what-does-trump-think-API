451f693e63c80ce580ef02a11a89a08ea060f1b1
Sean-Birdsall <sbirdsall0312@gmail.com>
2017-02-14 12:51:18 -0600

    Integrated twitter API to get recent Trump tweets

--- public/main.js
+++ public/main.js
@@ -6,22 +6,33 @@ mainController.$inject = ['$http'];
 function mainController($http){
   var main = this;
 
-  main.personalQuote = null;
-  main.randomQuote = null;
+  main.getTweet = function(){
 
-  main.score = 0;
+    $http.get('/tweets')
+      .then(function(res){
+        main.personalQuote = null;
+        main.randomQuote = null;
+        // console.log(res.data[Math.floor(getRandomInt(0, 20))].text);
+        main.tweet = res.data[Math.floor(getRandomInt(0, 20))].text;
+      }, function(err){
+        if (err){
+          console.log(err);
+        }
+      });
+
+  }
 
   main.increaseScore = function() {
     $http.get('/random-quote')
       .then(function(res){
         main.personalQuote = null;
+        main.tweet = null;
         main.randomQuote = res.data.message;
-        console.log(res.data.message);
       }, function(err){
         if (err){
           console.log(err);
         }
-      })
+      });
   }
 
   main.getPersonalQuote = function(){
@@ -30,15 +41,24 @@ function mainController($http){
       $http.get(`/personal-quote?q=${main.name}`)
         .then(function(res){
           main.randomQuote = null;
+          main.tweet = null;
           main.personalQuote = res.data.message;
         }, function(err){
           if (err){
             console.log(err);
           }
-        })
+        });
+    } else {
+      alert("Please enter your name in the textbox provided");
     }
 
 
+  };
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 
 }
--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 var express = require('express');
 var request = require('request');
+var lame = require('./lame');
 var PORT = process.env.PORT || 8080;
+var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key: lame.consumer_key,
+  consumer_secret: lame.consumer_secret,
+  access_token: lame.access_token,
+  access_token_secret: lame.access_token_secret
+});
 
 var app = express();
 
 app.use(express.static('public'));
+
+app.get('/tweets', (req, res) => {
+  T.get('statuses/user_timeline', { screen_name: '@realDonaldTrump'}, function(err, data, response) {
+    res.send(data);
+  })
+})
 
 app.get('/random-quote', (req, res) =>{
   request('https://api.whatdoestrumpthink.com/api/v1/quotes/random', (err, response, body) => {
     if (err){
       console.log('API Error');
     }
-
     res.send(body);
   })
-
 })
 
 app.get('/personal-quote', (req, res) =>{
@@ -22,9 +35,7 @@ app.get('/personal-quote', (req, res) =>{
     if (err){
       console.log(err);
     }
-
     res.send(body);
-
   })
 })
 
